a21a101d050e348af8556673af2f0ec9e07e8009
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-23 21:49:34 +0400

    list_display has been added to represent the BookInstance list. Added the built-in list of the Book list to the Author list view

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,26 +3,31 @@ from .models import Author, Genre, Book, BookInstance
 
 # Register your models here.
 
+class BookInline(admin.StackedInline):
+    model = Book
 
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
     fields = ["first_name", "last_name", ("date_of_birth", "date_of_death")]
+    inlines = [BookInline]
 
 
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
 
+
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
+    list_display = ("title", "author", "display_genre")
     inlines = [BooksInstanceInline]
 
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ("book", "status", "due_back", "id")
     list_filter = ("status", "due_back")
 
     fieldsets = (
